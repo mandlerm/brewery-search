59bb2c339512510794d28c1a59548233e1fdc792
mandlerm <amaya.melissa@gmail.com>
2019-07-10 21:12:31 -0500

    prettier and eslint

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js
+  </code> and save to reload.
         </p>
         <a
-          className="App-link"
+       className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
