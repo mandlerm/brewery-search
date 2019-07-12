201c201de05676bc51e4478b31f1b487fe34b06d
mandlerm <amaya.melissa@gmail.com>
2019-07-11 19:43:16 -0500

    remove unneeded files

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/style.css';
+import BrewerySearch from './BrewerySearch';
+
+ReactDOM.render(<BrewerySearch />, document.getElementById('root'));
