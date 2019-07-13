0948840ad24a34324b4bc6b0f1667eb7e303f765
mandlerm <amaya.melissa@gmail.com>
2019-07-13 18:30:07 -0500

    some styling around the list items

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -2,41 +2,53 @@ import React from 'react';
 import '../css/style.css';
 
 const BreweryList = props => {
-  const breweries = props.brewList.map(brewery => (
-    <div className="ui list raised segment">
-      <div className="item">
-        {/* <i className="users icon"></i> */}
-        <div className="content">{brewery.name}</div>
-      </div>
-
-      <div className="item">
-        <i className="info circle icon"></i>
-        <div className="content">{brewery.brewery_type}</div>
-      </div>
-
-      <div className="item">
-        <i className="marker icon"></i>
-        <div className="content">{brewery.street}</div>
-        <div className="content tab-left">
-          {brewery.city} {brewery.state}, {brewery.postal_code}
+  const breweries = props.brewList.map(
+    ({
+      id,
+      name,
+      brewery_type,
+      street,
+      city,
+      state,
+      postal_code,
+      website_url,
+    }) => (
+      <div key={id} className="ui list raised segment">
+        <div className="item">
+          <div className="content">
+            <h1>{name}</h1>
+          </div>
         </div>
-      </div>
 
-      {/* <div className="item">
+        <div className="item">
+          <i className="info circle icon"></i>
+          <div className="content">Type: {brewery_type}</div>
+        </div>
+
+        <div className="item">
+          <i className="marker icon"></i>
+          <div className="content">{street}</div>
+          <div className="content tab-left">
+            {city}, {state} {postal_code}
+          </div>
+        </div>
+
+        {/* <div className="item">
         <i className="phone icon"></i>
         <div className="content">
           <a href={brewery.website_url}>{brewery.phone}</a>
         </div>
       </div> */}
 
-      <div className="item">
-        <i className="linkify icon"></i>
-        <div className="content">
-          <a href={brewery.website_url}>{brewery.website_url}</a>
+        <div className="item">
+          <i className="linkify icon"></i>
+          <div className="content">
+            <a href={website_url}>{website_url}</a>
+          </div>
         </div>
       </div>
-    </div>
-  ));
+    )
+  );
 
   console.log(props.brewList);
   return <div className="brewery-list">{breweries}</div>;
