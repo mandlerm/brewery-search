13750704c3aff132f9bb4a34a551b08e8675518e
mandlerm <amaya.melissa@gmail.com>
2019-07-13 13:12:27 -0500

    search bar styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,21 +3,10 @@ import StateSelector from './StateSelector';
 import '../css/style.css';
 
 class SearchBar extends React.Component {
-  onFormSubmit(event) {
-    event.preventDefault();
-
-    console.log('I submitted');
-  }
-
   render() {
     return (
       <div className="search ui container">
-        <form onSubmit={this.onFormSubmit}>
-          <StateSelector />
-          <button className="ui primary button search-button" type="submit">
-            Show me the brewery!
-          </button>
-        </form>
+        <StateSelector button_message="Show me some breweries!" />
       </div>
     );
   }
