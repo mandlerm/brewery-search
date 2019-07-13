5f0b4cbc82d20f4f0d709532da41293c92a293dd
mandlerm <amaya.melissa@gmail.com>
2019-07-13 16:34:32 -0500

    made call back arrow function for this binding on axios call

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 import axios from 'axios';
 import Welcome from './Welcome';
@@ -5,15 +6,17 @@ import SearchBar from './SearchBar';
 import '../css/style.css';
 
 class App extends React.Component {
-  async onSearchSubmit(location) {
+  state = { brewery_listings: [] };
+
+  onSearchSubmit = async location => {
     const response = await axios.get(
       `https://api.openbrewerydb.org/breweries`,
       {
         params: { by_state: location },
       }
     );
-    console.log(response.data);
-  }
+    this.setState({ brewery_listings: response.data });
+  };
 
   render() {
     return (
@@ -23,6 +26,7 @@ class App extends React.Component {
           button_message="Show me some breweries!"
           onSubmit={this.onSearchSubmit}
         />
+        Found: {this.state.brewery_listings.length} breweries
       </div>
     );
   }
