45ad04403252834f8f790bd7a69d76e6add5ee4e
mandlerm <amaya.melissa@gmail.com>
2019-07-13 20:16:07 -0500

    added error page for router

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Error = () => (
+  <div>
+    <h2>Not found!!!</h2>
+  </div>);
+
+export default Error;
--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import ShowBrewery from './ShowBrewery';
+import Error from './Error';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/p" component={ShowBrewery} />
+      <Route path="/brewery/:name" component={ShowBrewery} />
+      <Route component={Error} />
     </Switch>
   </BrowserRouter>
 );
