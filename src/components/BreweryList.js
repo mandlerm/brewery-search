90b20502108fcbe1a72417d796628bcb3d9d6dc9
mandlerm <amaya.melissa@gmail.com>
2019-07-13 20:58:36 -0500

    refactor of brewerylist out to a listitem, with destructuring

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/style.css';
+import ListItem from './ListItem';
 
 class BreweryList extends React.Component {
   onClickBrewer = id => {
@@ -11,63 +12,13 @@ class BreweryList extends React.Component {
   };
 
   render() {
-    const breweries = this.props.brewList.map(
-      ({
-        id,
-        name,
-        brewery_type,
-        street,
-        city,
-        state,
-        postal_code,
-        website_url,
-      }) => (
-        <span
-          onClick={() => this.onClickBrewer({ id })}
-          className="list-item"
-          key={id}
-        >
-          <div className="ui list raised segment">
-            <div className="item">
-              <div className="content">
-                <h1>{name}</h1>
-              </div>
-            </div>
-
-            <div className="item">
-              <i className="info circle icon"></i>
-              <div className="content">Type: {brewery_type}</div>
-            </div>
-
-            <div className="item">
-              <i className="marker icon"></i>
-              <div className="content">{street}</div>
-              <div className="content tab-left">
-                {city}, {state} {postal_code}
-              </div>
-            </div>
-
-            <div className="item">
-              <i className="linkify icon"></i>
-              <div className="content">
-                <a href={website_url}>{website_url}</a>
-              </div>
-            </div>
-
-            <button
-              onClick={this.onClickBrewer.bind(this, { id })}
-              type="button"
-              className="ui right labeled icon button more"
-            >
-              <i className="right arrow icon"></i>
-              See more info
-            </button>
-          </div>
-        </span>
-      )
+    return (
+      <div>
+        {this.props.brewList.map(brewer => (
+          <ListItem brew={brewer} />
+        ))}
+      </div>
     );
-
-    return <div className="brewery-list">{breweries}</div>;
   }
 }
 
---
+++ src/components/ListItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class ListItem extends React.Component {
+  render() {
+    const {
+      id,
+      name,
+      brewery_type,
+      street,
+      city,
+      state,
+      postal_code,
+      website_url,
+    } = this.props.brew;
+
+    return (
+      <span
+        onClick={() => this.onClickBrewer({ id })}
+        className="list-item"
+        key={id}
+      >
+        <div className="ui list raised segment">
+          <div className="item">
+            <div className="content">
+              <h1>{name}</h1>
+            </div>
+          </div>
+
+          <div className="item">
+            <i className="info circle icon"></i>
+            <div className="content">Type: {brewery_type}</div>
+          </div>
+
+          <div className="item">
+            <i className="marker icon"></i>
+            <div className="content">{street}</div>
+            <div className="content tab-left">
+              {city}, {state} {postal_code}
+            </div>
+          </div>
+
+          <div className="item">
+            <i className="linkify icon"></i>
+            <div className="content">
+              <a href={website_url}>{website_url}</a>
+            </div>
+          </div>
+
+          <button
+            onClick={this.onClickBrewer.bind(this, { id })}
+            type="button"
+            className="ui right labeled icon button more"
+          >
+            <i className="right arrow icon"></i>
+            See more info
+          </button>
+        </div>
+      </span>
+    );
+  }
+}
+
+export default ListItem;
